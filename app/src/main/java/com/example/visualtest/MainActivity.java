3a74de577a91db7ccf2f6c14352a7fc84297a409
ntw516 <ntw516@163.com>
2022-05-17 14:22:09 +0800

    Project create

---
+++ app/src/main/java/com/example/visualtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.visualtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import org.bytedeco.openpose.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
